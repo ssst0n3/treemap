30e16d574e524362904883c1819f7aceeb02285b
st0n3 <ssst0n3@gmail.com>
2020-09-24 03:55:27 +0800

    修改：     api/v1/node.go

--- api/v1/node.go
+++ api/v1/node.go
@@ -1,11 +1,13 @@
 package v1
 
 import (
+	"bytes"
 	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/ssst0n3/lightweight_api"
 	"github.com/ssst0n3/treemap/database"
 	"github.com/ssst0n3/treemap/model/node"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -40,37 +42,47 @@ func TreeNodes(c *gin.Context) {
 }
 
 func CreateNode(c *gin.Context) {
-	var body node.CreateBody
-	if err := c.BindJSON(&body); err != nil {
-		lightweight_api.HandleStatusBadRequestError(c, err)
+	if body, err := ioutil.ReadAll(c.Request.Body); err != nil {
+		lightweight_api.HandleInternalServerError(c, err)
 		return
-	}
-	if err := NodeResource.MustResourceExistsById(c, int64(body.Parent)); err != nil {
-		lightweight_api.HandleStatusBadRequestError(c, err)
-		return
-	}
-	NodeResource.CreateResource(c, &node.Node{}, "", nil, func(id int64) {
-		var children string
-		if err := database.Conn.OrmShowObjectOnePropertyByIdUsingJsonBind(node.TableNameNode, node.ColumnNameNodeChildren, int64(body.Parent), &children); err != nil {
-			lightweight_api.HandleInternalServerError(c, err)
+	} else {
+		var createBody node.CreateBody
+		if err := json.Unmarshal(body, &createBody); err != nil {
+			lightweight_api.HandleStatusBadRequestError(c, err)
 			return
-		} else {
-			var childrenIdList []uint
-			if err := json.Unmarshal([]byte(children), &childrenIdList); err != nil {
+		}
+		if err := c.BindJSON(&createBody); err != nil {
+			lightweight_api.HandleStatusBadRequestError(c, err)
+			return
+		}
+		if err := NodeResource.MustResourceExistsById(c, int64(createBody.Parent)); err != nil {
+			lightweight_api.HandleStatusBadRequestError(c, err)
+			return
+		}
+		c.Request.Body = ioutil.NopCloser(bytes.NewReader(body))
+		NodeResource.CreateResource(c, &node.Node{}, "", nil, func(id int64) {
+			var children string
+			if err := database.Conn.OrmShowObjectOnePropertyByIdUsingJsonBind(node.TableNameNode, node.ColumnNameNodeChildren, int64(createBody.Parent), &children); err != nil {
 				lightweight_api.HandleInternalServerError(c, err)
 				return
 			} else {
-				childrenIdList = append(childrenIdList, uint(id))
-				if newChildren, err := json.Marshal(childrenIdList); err != nil {
+				var childrenIdList []uint
+				if err := json.Unmarshal([]byte(children), &childrenIdList); err != nil {
 					lightweight_api.HandleInternalServerError(c, err)
 					return
 				} else {
-					if err := database.Conn.UpdateObjectSingleColumnById(int64(body.Parent), node.TableNameNode, node.ColumnNameNodeChildren, newChildren); err != nil {
+					childrenIdList = append(childrenIdList, uint(id))
+					if newChildren, err := json.Marshal(childrenIdList); err != nil {
 						lightweight_api.HandleInternalServerError(c, err)
 						return
+					} else {
+						if err := database.Conn.UpdateObjectSingleColumnById(int64(createBody.Parent), node.TableNameNode, node.ColumnNameNodeChildren, newChildren); err != nil {
+							lightweight_api.HandleInternalServerError(c, err)
+							return
+						}
 					}
 				}
 			}
-		}
-	})
+		})
+	}
 }
