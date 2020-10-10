7b906e136f72dbda1bb9b1f82995bc7b4677dc76
st0n3 <ssst0n3@gmail.com>
2020-10-10 10:33:16 +0800

    修改：     api/v1/router.go 	修改：     go.mod

--- api/v1/router.go
+++ api/v1/router.go
@@ -2,10 +2,24 @@ package v1
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/ssst0n3/lightweight_api/middleware"
 )
 
 func InitRouter(router *gin.Engine) {
 	nodeGroup := router.Group(NodeResource.BaseRelativePath)
+	{
+		nodeGroup.GET("/root", ListRootNodes)
+		nodeGroup.GET("/root/:id", TreeNodes)
+		nodeGroup.GET("/children/:id", NodeResource.ShowResource)
+		nodeGroup.POST("/root", CreateRoot)
+		nodeGroup.POST("/child", CreateChild)
+		nodeGroup.DELETE("/child/:id", NodeResource.DeleteResource)
+		nodeGroup.PUT("/node/:id", UpdateNode)
+	}
+}
+
+func InitRouterWithJwtAdmin(router *gin.Engine) {
+	nodeGroup := router.Group(NodeResource.BaseRelativePath, middleware.JwtAdmin())
 	{
 		nodeGroup.GET("/root", ListRootNodes)
 		nodeGroup.GET("/root/:id", TreeNodes)
