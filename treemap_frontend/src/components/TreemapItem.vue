ef6a8f782a0b4176d864e0a90bcd477875014042
st0n3 <ssst0n3@gmail.com>
2020-10-27 19:03:42 +0800

    修改：     ../docker-compose.yml 	修改：     package.json 	修改：     src/components/TreemapItem.vue

--- treemap_frontend/src/components/TreemapItem.vue
+++ treemap_frontend/src/components/TreemapItem.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <b-btn @click="compress_btn_clicked" class="ml-3"><i :class="compress?'fas fa-compress-alt':'fas fa-expand-alt'"></i></b-btn>
+    <div class="position-absolute" style="z-index: 3">
+      <b-btn @click="compress_btn_clicked" class="ml-3"><i :class="compress?'fas fa-compress-alt':'fas fa-expand-alt'"></i></b-btn>
+    </div>
     <js-mind :key="refresh_key" :values="mind" :height="`${height}px`" ref="js_mind"/>
   </div>
 </template>
